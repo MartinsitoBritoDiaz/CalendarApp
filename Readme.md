a2e52f0c875b6b0ab2c1fb55746d7c8297d49e59
MartinsitoBritoDiaz <martinsitobd@gmail.com>
2023-04-19 23:57:56 -0400

    feat: readme update

--- Readme.md
+++ Readme.md
@@ -1,6 +1,5 @@
 
-# Project Title
-Calendar APP
+# Calendar APP
 
 ## Demo link:
 Access my site at [Working](https://github.com/MartinsitoBritoDiaz/CalendarApp.git)
@@ -16,27 +15,38 @@ Access my site at [Working](https://github.com/MartinsitoBritoDiaz/CalendarApp.g
 - [About Me](#about-me)
 
 ## About The App
-Calendar APP is an app that ...
+Calendar APP is an app that allows you to create a different kinds of events an manage them with a beautifull user interface. 
 
+I created this project as part of this udemy course:  
+- [React: De cero a experto ( Hooks y MERN ) ](https://www.udemy.com/course/react-cero-experto/) 
 ## Screenshots
 
-`![Writing](https://unsplash.com/photos/VBPzRgd7gfc)`
+### Login Page
+![imagen](https://user-images.githubusercontent.com/54644026/233251847-04143029-728c-4adb-99d8-53937a5db039.png)
 
-Picture by [Kelly Sikkema](https://unsplash.com/@kellysikkema)
+### Register Page
+![imagen](https://user-images.githubusercontent.com/54644026/233251946-9750e1b3-9783-4234-862c-b05d79f7255a.png)
+
+### Main Page
+![imagen](https://user-images.githubusercontent.com/54644026/233252245-66a13c8e-9128-4b4f-8b3d-6fd7b6a05a7e.png)
+
+### Event Creation Page
+![imagen](https://user-images.githubusercontent.com/54644026/233252369-3d9fbeea-3d2a-4dce-9c26-1e82845db537.png)
+
 
 ## Technologies
-I used `html`, `css`, ...
+I used `React JS`, `Bootstrap` and `Redux`
 
 ## Setup
+- must wait beacuse the back end of this project is still in proccess, so whenever will be ready, I let you know to run properly, but if you want to try do it.
 - download or clone the repository
-- run `npm install`
-- ...
+- run `npm install` or `yarn`
 
 ## Approach
-I adopted the `BEM` naming style for my css class names and ...
+I adopted the `BEM` naming style for my css class names.
 
 ## Status
-[name of project] is still in progress. `Version 2` will be out soon.
+- [Calendar App]() is still in progress. `Back End Project` will be out soon.
 
 ## About me
 List of contriubutors:
--- src/app/components/CalendarModal.jsx
+++ src/app/components/CalendarModal.jsx
@@ -1,15 +1,14 @@
-import "sweetalert2/dist/sweetalert2.min.css";
+import 'sweetalert2/dist/sweetalert2.min.css';
 
-import Modal from "react-modal";
+import Modal from 'react-modal';
 
-import "react-datepicker/dist/react-datepicker.css";
-import DatePicker from "react-datepicker";
-import { useUIStore } from "../../hooks";
-import { customStyles } from "../../helper";
-import { useCustomForm } from "../../hooks";
+import 'react-datepicker/dist/react-datepicker.css';
+import DatePicker from 'react-datepicker';
+import { customStyles } from '../../helpers';
+import { useCustomForm } from '../../hooks';
 
 
-Modal.setAppElement("#root");
+Modal.setAppElement('#root');
 
 export function CalendarModal() {
 
@@ -38,76 +37,76 @@ export function CalendarModal() {
       isOpen={isDateModalOpen}
       onRequestClose={onCloseModal}
       style={customStyles}
-      className="modal"
-      overlayClassName="modal-fondo"
+      className='modal'
+      overlayClassName='modal-fondo'
       closeTimeoutMS={200}
     >
-      <div className="d-flex justify-content-between">
-        <h1 className="fs-3 my-3"> New Event </h1>
-        <button className="btn btn-lg btn--closeModal" onClick={onCloseModal}>
-          <i className="fas fa-xmark"></i>
+      <div className='d-flex justify-content-between'>
+        <h1 className='fs-3 my-3'> New Event </h1>
+        <button className='btn btn-lg btn--closeModal' onClick={onCloseModal}>
+          <i className='fas fa-xmark'></i>
         </button>
       </div>
 
-      <form className="container " onSubmit={onSubmit}>
-        <div className="form-group mb-2">
+      <form className='container ' onSubmit={onSubmit}>
+        <div className='form-group mb-2'>
           <label>Start date</label>
           <DatePicker
             selected={formValues.start}
-            className="form-control"
-            dateFormat="Pp"
+            className='form-control'
+            dateFormat='Pp'
             showTimeSelect
-            onChange={(event) => onDateChange(event, "start")}
+            onChange={(event) => onDateChange(event, 'start')}
           />
         </div>
 
-        <div className="form-group mb-2">
+        <div className='form-group mb-2'>
           <label>End date</label>
           <DatePicker
             minDate={formValues.start}
             selected={formValues.end}
-            className="form-control"
-            dateFormat="Pp"
+            className='form-control'
+            dateFormat='Pp'
             showTimeSelect
-            onChange={(event) => onDateChange(event, "end")}
+            onChange={(event) => onDateChange(event, 'end')}
           />
         </div>
 
         <hr />
-        <div className="form-group mb-2">
+        <div className='form-group mb-2'>
           <label>Title</label>
           <input
-            type="text"
+            type='text'
             className={`form-control ${titleClass}`}
-            placeholder="Birthday"
-            name="title"
-            autoComplete="off"
+            placeholder='Birthday'
+            name='title'
+            autoComplete='off'
             value={formValues.title}
             onChange={onInputChange}
           />
-          <small id="emailHelp" className="form-text text-light">
+          <small id='emailHelp' className='form-text text-light'>
             Short description
           </small>
         </div>
 
-        <div className="form-group mb-2">
+        <div className='form-group mb-2'>
           <textarea
-            type="text"
-            className="form-control"
-            placeholder="Notes"
-            rows="5"
-            name="notes"
+            type='text'
+            className='form-control'
+            placeholder='Notes'
+            rows='5'
+            name='notes'
             value={formValues.notes}
             onChange={onInputChange}
           ></textarea>
-          <small id="emailHelp" className="form-text text-muted">
+          <small id='emailHelp' className='form-text text-muted'>
             Aditional information
           </small>
         </div>
 
-        <div className="d-flex justify-content-end">
-          <button type="submit" className="btn btn--custom btn-block align-left">
-            <i className="far fa-save"></i>
+        <div className='d-flex justify-content-end'>
+          <button type='submit' className='btn btn--custom btn-block align-left'>
+            <i className='far fa-save'></i>
             <span> Save</span>
           </button>
         </div>
